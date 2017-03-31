b45580e9e70c456998da5412102ac9f9b7b48ccb
llv <llv@feip.co>
2017-04-01 01:44:51 +1000

    basic layout

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+  pagination: '.swiper-pagination',
+  paginationClickable: true,
+  spaceBetween: 30,
+  effect: 'fade',
+  centeredSlides: true,
+  autoplay: 2500,
+  autoplayDisableOnInteraction: false,
+});

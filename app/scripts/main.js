bb549c2c8a02c08fc48df86140163b06dc9afa92
llv <llv@feip.co>
2017-04-01 23:40:31 +1000

    add favorites slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.swiper-container', {
+var slider = new Swiper('.slider', {
   pagination: '.swiper-pagination',
   paginationClickable: true,
   spaceBetween: 30,
@@ -6,4 +6,31 @@ var swiper = new Swiper('.swiper-container', {
   centeredSlides: true,
   autoplay: 2500,
   autoplayDisableOnInteraction: false,
+});
+
+
+if (window.window.innerWidth <= 600) {
+  document.querySelector('.swiper-button-next').style.display = 'none';
+  document.querySelector('.swiper-button-prev').style.display = 'none';
+  // document.querySelector('.swiper-pagination-bullets').style.display  = 'none';
+}
+
+var favorites = new Swiper('.favorites', {
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  slidesPerView: 5,
+  paginationClickable: true,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      pagination: '',
+    },
+  }
 });
